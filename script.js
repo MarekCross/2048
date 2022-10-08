57035e1064d9ba055b4a5d27a91e3fe0d7167444
Marek Krzyżowski <themarkcross@gmail.com>
2022-10-08 19:56:02 +0200

    got same bug and trying to fix this s....

--- script.js
+++ script.js
@@ -4,11 +4,9 @@ import Tile from './Tile.js';
 const gameBoard = document.getElementById('game-board');
 
 const grid = new Grid(gameBoard);
-console.log(grid.randomEmptyCell());
 grid.randomEmptyCell().tile = new Tile(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
 setupInput();
-console.log(grid.cellsByCalumn);
 
 function setupInput() {
   window.addEventListener('keydown', handleInput, { once: true });
@@ -22,7 +20,7 @@ function handleInput(e) {
     case 'ArrowDown':
       moveDown();
       break;
-    case 'ArropLeft':
+    case 'ArrowLeft':
       moveLeft();
       break;
     case 'ArrowRight':
@@ -30,21 +28,41 @@ function handleInput(e) {
       break;
     default:
       setupInput();
-      break;
+      return;
   }
   setupInput();
 }
 function moveUp() {
-  slideTiles();
+  return slideTiles(grid.cellsByColumn);
+}
+function moveDown() {
+  return slideTiles(grid.cellsByColumn.map(column => [...column].reverse()));
+}
+function moveLeft() {
+  return slideTiles(grid.cellsByRow);
+}
+function moveRight() {
+  return slideTiles(grid.cellsByRow.map(row => [...row].reverse()));
 }
 
 function slideTiles(cells) {
   cells.forEach(group => {
     for (let i = 1; i < group.lenght; i++) {
       const cell = group[i];
+      if (cell.tile == null) continue;
+      let lastvalidCell;
       for (let j = i - 1; j >= 0; j--) {
         const movetoCell = group[j];
         if (!movetoCell.canAccept(cell.tile)) break;
+        lastvalidCell = movetoCell;
+      }
+      if (lastvalidCell != null) {
+        if (lastvalidCell.tile != null) {
+          lastvalidCell.mergeTile = cell.tile;
+        } else {
+          lastvalidCell.tile = cell.tile;
+        }
+        cell.tile = null;
       }
     }
   });
